1b93d2e929856c4c92e67639fd224d7558186fcb
ShadOhh <Darkassasinvx@gmail.com>
2024-01-10 09:02:44 -0500

    problem1 done

--- src/Week1/Problems.java
+++ src/Week1/Problems.java
@@ -1,2 +1,23 @@
-package Week1;public class Problems {
+package Week1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problems {
+
+    public static int[] problem1(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
 }
+
--- src/Week1/TestDrive.java
+++ src/Week1/TestDrive.java
@@ -1,2 +1,30 @@
-package Week1;public class TestDrive {
+package Week1;
+
+import java.beans.beancontext.BeanContextServiceProviderBeanInfo;
+import java.util.Arrays;
+
+public class TestDrive {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11};
+        int target = 9;
+        int[] expected = {0,1};
+
+
+        System.out.println(Arrays.equals(expected, Problems.problem1(nums,target)));
+
+        int[] nums2 = {3,2,4};
+        target = 6;
+        int[] expected2 = {1,2};
+
+        System.out.println(Arrays.equals(expected2,Problems.problem1(nums2,target)));
+
+        int[] nums3 = {3,3};
+        int[] expected3 = {0,1};
+
+        System.out.println(Arrays.equals(expected3,Problems.problem1(nums3,target)));
+
+    }
+
+
+
 }
